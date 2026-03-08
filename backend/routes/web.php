f8b2e5e7dc1059a5f116537030005c6fb3ece95e
Shaurya Chawan <shauryachawan4646.dev@gmil.com>
2026-03-09 01:54:55 +0530

    chore: removed the entire react.js from the laravel backend folder. thus having the new frontend as next.js

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -2,10 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/{any}', function () {
-    return view('app');
-})->where('any', '^(?!api).*$');
-
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return ['message' => 'Restaurant Dashboard API Configuration Server is Live.'];
+});
